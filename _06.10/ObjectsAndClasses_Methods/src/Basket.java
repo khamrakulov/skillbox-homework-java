9bd74dd743dcb751364b6e365d7c5ee5341ab351
Kamron Khamrakulov <khamrakulovkamron@gmail.com>
2023-08-09 19:36:14 +0500

    added static variables and static methods

--- _06.10/ObjectsAndClasses_Methods/src/Basket.java
+++ _06.10/ObjectsAndClasses_Methods/src/Basket.java
@@ -1,14 +1,17 @@
 public class Basket {
 
-    private static int count = 0;
-    private String items = "";
-    private int totalPrice = 0;
+    private int countItemsBasket = 0;
+    private String itemsBasket = "";
+    private int totalPriceBasket = 0;
     private int limit;
     private double totalWeight = 0;
 
+    // static variables
+    private static int CountAllItemsOfBaskets = 0;
+    private static int totalPriceAllItems = 0;
+
     public Basket() {
-        increaseCount(1);
-        items = "Список товаров:";
+        itemsBasket = "Список товаров:";
         this.limit = 1000000;
     }
 
@@ -17,35 +20,44 @@ public class Basket {
         this.limit = limit;
     }
 
-    public Basket(String items, int totalPrice) {
+    public Basket(String itemsBasket, int totalPriceBasket) {
         this();
-        this.items = this.items + items;
-        this.totalPrice = totalPrice;
+        this.itemsBasket = this.itemsBasket + itemsBasket;
+        this.totalPriceBasket = totalPriceBasket;
     }
 
-    public static int getCount() {
-        return count;
+    public int getcountItemsBasket() {
+        return countItemsBasket;
     }
 
-    public static void increaseCount(int count) {
-        Basket.count = Basket.count + count;
+    public void increaseCountItemsBasket() {
+        increaseCountItemsBasket(1);
+        increaseCountAllItems(1);
+    }
+
+    public void increaseCountItemsBasket(int countItemsBasket) {
+        this.countItemsBasket += countItemsBasket;
+        increaseCountAllItems(countItemsBasket);
     }
 
     public void add(String name, int price) {
         add(name, price, 1, 50.0);
     }
 
-    public void add(String name, int price, int count) {
-        add(name, price, count, 50.0);
+    public void add(String name, int price, int countItemsBasket) {
+        add(name, price, countItemsBasket, 50.0);
     }
 
-    public void add(String name, int price, int count, double weight) {
+    public void add(String name, int price, int countItemsBasket, double weight) {
+        increaseTotalPriceBaskets(price*countItemsBasket);
+        increaseCountItemsBasket(countItemsBasket);
+
         boolean error = false;
         if (contains(name)) {
             error = true;
         }
 
-        if (totalPrice + count * price >= limit) {
+        if (totalPriceBasket + countItemsBasket * price >= limit) {
             error = true;
         }
 
@@ -54,34 +66,55 @@ public class Basket {
             return;
         }
 
-        items = items + "\n" + name + " - " +
-            count + " шт. " + "(" + weight * count + " кг) - " + price;
-        totalPrice = totalPrice + count * price;
-        totalWeight = totalWeight + count * weight;
+        itemsBasket = itemsBasket + "\n" + name + " - " +
+            countItemsBasket + " шт. " + "(" + weight * countItemsBasket + " кг) - " + countItemsBasket * price;
+        totalPriceBasket = totalPriceBasket + countItemsBasket * price;
+        totalWeight = totalWeight + countItemsBasket * weight;
     }
 
     public void clear() {
-        items = "";
-        totalPrice = 0;
+        itemsBasket = "";
+        totalPriceBasket = 0;
     }
 
     public int getTotalPrice() {
-        return totalPrice;
+        return totalPriceBasket;
     }
     public double getTotalWeight() {
         return totalWeight;
     }
 
     public boolean contains(String name) {
-        return items.contains(name);
+        return itemsBasket.contains(name);
     }
 
     public void print(String title) {
         System.out.println(title);
-        if (items.isEmpty()) {
+        if (itemsBasket.isEmpty()) {
             System.out.println("Корзина пуста");
         } else {
-            System.out.println(items);
+            System.out.println(itemsBasket);
         }
+    }
+
+    // static methods
+    public static int getCountAllItems() {
+        return Basket.CountAllItemsOfBaskets;
+    }
+
+    private static void increaseCountAllItems() {
+        increaseCountAllItems(1);
+    }
+
+    private static void increaseCountAllItems(int count) {
+        Basket.CountAllItemsOfBaskets += count;
+    }
+
+    public static int getTotalPriceBaskets() {
+        return Basket.totalPriceAllItems;
+    }
+
+    private static void increaseTotalPriceBaskets(int price) {
+        Basket.totalPriceAllItems += price;
     }
 }
--- _06.10/ObjectsAndClasses_Methods/src/Main.java
+++ _06.10/ObjectsAndClasses_Methods/src/Main.java
@@ -1,8 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        Basket basket = new Basket();
-        basket.add("Milk", 40, 3);
-        basket.print("Milk");
+        Basket basket1 = new Basket();
+        basket1.add("Milk", 40, 3);
+        basket1.add("Tea", 30, 2);
+
+        Basket basket2 = new Basket();
+        basket2.add("Milk", 40, 3);
+
+        basket1.print("basket1");
+        System.out.println("\n");
+        basket2.print("basket2");
+        System.out.println("\n");
+
+        System.out.println("Total items:");
+        System.out.println(Basket.getCountAllItems());
+        System.out.println("Total Price:");
+        System.out.println(Basket.getTotalPriceBaskets());
     }
 }
